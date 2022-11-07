7e1470711ad070fe450680979e3b1f23e310c96e
ZigNistus <sadilow.nikita@yandex.ru>
2022-11-07 19:45:22 +0400

    init

---
+++ src/mocks/mocks.js
@@ -0,0 +1,42 @@
+const Mocks = () => {
+    const mocks = [{
+        "id": 1,
+        "name": 'Первый',
+        "phone": '+7(111)111-11-11',
+        "favorite": 'favorite'
+    },
+    {
+        "id": 2,
+        "name": 'Второй',
+        "phone": '+7(222)222-22-22',
+        "favorite": 'none'
+    },
+    {
+        "id": 3,
+        "name": 'Третий',
+        "phone": '+7(333)333-33-33',
+        "favorite": 'none'
+    },
+    {
+        "id": 4,
+        "name": 'Четвертый',
+        "phone": '+7(444)444-44-44',
+        "favorite": 'favorite'
+    },
+    {
+        "id": 5,
+        "name": 'Пятый',
+        "phone": '+7(555)555-55-55',
+        "favorite": 'favorite'
+    },
+    {
+        "id": 6,
+        "name": 'Шестой',
+        "phone": '+7(666)666-66-66',
+        "favorite": 'none'
+    }]
+
+    return mocks
+}
+
+export default Mocks
